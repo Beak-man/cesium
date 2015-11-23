11a6b505539062312e5a55fb773928eb1469e245
OmarDelaa <omar.delaa@outlook.fr>
2015-11-23 17:32:19 +0100

    modifs MarkerMove

--- Apps/PlanetaryCesiumViewer/Cesium/Widgets/MarkerMove/MarkerMove.js
+++ Apps/PlanetaryCesiumViewer/Cesium/Widgets/MarkerMove/MarkerMove.js
@@ -17,89 +17,10 @@ define([
             ScreenSpaceEventType) { 
                 "use strict";
 
-            /*    var selectMarker = function(toolbar, container, scene){
-
-                    var lastEntity;
-					var lastEntityMoved;
-                    var element;
-                    var ellipsoid = scene.globe.ellipsoid;
-					
-                    var handler = new ScreenSpaceEventHandler(scene.canvas);
-                    
-                    
-                    handler.setInputAction(function(click){
-
-                        var pickedObject = scene.pick(click.position);
-
-                        if (pickedObject) {
-							
-                           var entity = pickedObject.primitive.id;
-                            entity._billboard.color = new Color(1.0, 1.0, 0.0, 1.0);
-							
-							console.log(pickedObject);
-							
-							
-                            if (lastEntity && lastEntity != entity) { // deuxieme coups et plus
-                                lastEntity._billboard.color = new Color(1.0, 1.0, 1.0, 1.0);
-                                lastEntity = entity;
-								entity = null;
-								
-                            } else if (!lastEntity) {  // premier coups
-                                    lastEntity = entity;
-									entity = null;
-									
-                            } else if (lastEntity && lastEntity == entity) { // deuxieme coups et plus
-                                        lastEntity._billboard.color = new Color(1.0, 1.0, 0.0, 1.0);
-                                        lastEntity = entity;
-										entity = null;
-                                    }
-                            
-                            
-                            var handler = new ScreenSpaceEventHandler(scene.canvas);
-                            handler.setInputAction(function(click){
-                            
-                                var cartesian    = scene.camera.pickEllipsoid(click.position, ellipsoid);
-								var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-                                
-                                if (lastEntity) {
-									
-									
-									if (!lastEntity.properties.modifed_lat) lastEntity.addProperty("modifed_lat");
-									if (!lastEntity.properties.modifed_lon) lastEntity.addProperty("modifed_lon");
-									
-									lastEntityMoved = lastEntity;
-                                    lastEntity.position._value = cartesian;
-									
-									
-									lastEntity.properties.modifed_lat = CesiumMath.toDegrees(cartographic.latitude); 
-									lastEntity.properties.modifed_lon = CesiumMath.toDegrees(cartographic.longitude); ; 
-									
-									console.log(lastEntity.features);
-									
-                                    lastEntity._billboard.color      = new Color(0.0, 1.0, 0.0, 1.0);
-									lastEntityMoved._billboard.color = new Color(0.0, 1.0, 0.0, 1.0);
-									
-									// document.location = 'data:json/octet-stream,' + JSON.stringify(lastEntity.properties);
-									
-                                    entity = 'undefined'
-									lastEntity = null;
-									pickedObject = null;
-									
-									
-									var txtData = "toto";
-                                }
-                            }, ScreenSpaceEventType.RIGHT_CLICK);	
-							
-                        }    
-               }, ScreenSpaceEventType.LEFT_CLICK);
-            }*/
 
             var MarkerMove = function(toolbar, container, scene, viewer){
 				
-				
 			if(!window.sessionStorage.getItem("geoJsonData"))	window.sessionStorage.setItem("geoJsonData", {});
-				
-            //var sm = selectMarker(toolbar, container, scene);
 			
 			var viewModel   = new MarkerMoveViewModel(toolbar, container, scene, viewer);	
                 this._viewModel = viewModel;
@@ -116,10 +37,7 @@ define([
                 container.appendChild(element); 
 
                 knockout.applyBindings(viewModel, element);
-			   
-			   
-			   
-			   
+
             }
          return MarkerMove;
 });
--- Apps/PlanetaryCesiumViewer/Cesium/Widgets/MarkerMove/MarkerMoveViewModel.js
+++ Apps/PlanetaryCesiumViewer/Cesium/Widgets/MarkerMove/MarkerMoveViewModel.js
@@ -34,122 +34,86 @@ var _isActive = false;
 			if(_isActive ==false){
 			
 			        var lastEntity;
-					var lastEntityMoved;
-                    var element;
+					var entity = null;
                     var ellipsoid = scene.globe.ellipsoid;
 					
                     var handler = new ScreenSpaceEventHandler(scene.canvas);
-                    
-                    
                     handler.setInputAction(function(click){
 
-                        var pickedObject = scene.pick(click.position);
-                       // console.log(viewer.selectedEntity); 
-						//console.log(viewer.dataSources._dataSources[0].load);
+						entity = null;
+						var pickedObject = null;
+
+                         pickedObject = scene.pick(click.position);
+						
                         if (pickedObject) {
 							
-                           var entity = pickedObject.primitive.id;
+                            entity = pickedObject.primitive.id;
                             entity._billboard.color = new Color(1.0, 0.0, 0.0, 1.0);
 							
-							console.log(pickedObject);
-
-							var dataSource = new GeoJsonDataSource();
-							var entities = dataSource.entities.values;
-							
-                            if (lastEntity && lastEntity != entity) { // deuxieme coups et plus
+							if (!lastEntity) { // premier coups
+								lastEntity = entity;
+							} else if (lastEntity && lastEntity != entity) { 
                                 lastEntity._billboard.color = new Color(1.0, 1.0, 1.0, 1.0);
                                 lastEntity = entity;
-								entity = null;
-								
-                            } else if (!lastEntity) {  // premier coups
-                                    lastEntity = entity;
-									entity = null;
-									
-                            } else if (lastEntity && lastEntity == entity) { // deuxieme coups et plus
-                                        lastEntity._billboard.color = new Color(1.0, 1.0, 0.0, 1.0);
+
+                            } else if (lastEntity && lastEntity == entity) { 
+                                        lastEntity._billboard.color = new Color(1.0, 0.0, 0.0, 1.0);
                                         lastEntity = entity;
-										entity = null;
-                                    }
-                            
-                            
-                            var handler = new ScreenSpaceEventHandler(scene.canvas);
-                            handler.setInputAction(function(click){
-                           
+                                    } 
+                        }    
+               }, ScreenSpaceEventType.LEFT_CLICK);
+			   
+			   
+			  var handler2 = new ScreenSpaceEventHandler(scene.canvas);
+                     handler2.setInputAction(function(click){
 							
                                 var cartesian    = scene.camera.pickEllipsoid(click.position, ellipsoid);
 								var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-                                
-                                if (lastEntity) {
+								
+                                if (entity) {
 									
-									if (!lastEntity.properties.modifed_lat) lastEntity.addProperty("modifed_lat");
-									if (!lastEntity.properties.modifed_lon) lastEntity.addProperty("modifed_lon");
-									
-									lastEntityMoved = lastEntity;
-                                    lastEntity.position._value = cartesian;
-									
-									
-									lastEntity.properties.old_lat    = lastEntity.properties.center_lat; 
-									lastEntity.properties.old_lon    = lastEntity.properties.center_lon; 
-									
+									 entity.position._value = cartesian;
+
+									entity.properties.old_lat    = entity.properties.center_lat; 
+									entity.properties.old_lon    = entity.properties.center_lon; 
 									
 									 var longitudeRad = cartographic.longitude;
 		                             if (longitudeRad < 0){
 		                                  longitudeRad = longitudeRad + 2.0*Math.PI;
 		                             }	
-									lastEntity.properties.center_lat = CesiumMath.toDegrees(cartographic.latitude); 
-									lastEntity.properties.center_lon = CesiumMath.toDegrees(longitudeRad); 
+									entity.properties.center_lat = CesiumMath.toDegrees(cartographic.latitude); 
+									entity.properties.center_lon = CesiumMath.toDegrees(longitudeRad); 
 									
-                                    lastEntity._billboard.color      = new Color(0.0, 1.0, 0.0, 1.0);
-									lastEntityMoved._billboard.color = new Color(0.0, 1.0, 0.0, 1.0);
-									
-									//***************************** Pour enregistrer des données *********************************
-									// document.location = 'data:json/octet-stream,' + JSON.stringify(lastEntity.properties);
-									//********************************************************************************************
-									
-                                    entity = 'undefined'
+                                    entity._billboard.color      = new Color(0.0, 1.0, 0.0, 1.0);
+
 									lastEntity = null;
-									pickedObject = null;		
-									
                                 }
                             }, ScreenSpaceEventType.RIGHT_CLICK);
-                        }    
-               }, ScreenSpaceEventType.LEFT_CLICK);
-			
+			   
 			  _isActive = true;
-			  
-			 // console.log(document.getElementById('xxPrimeSave'));
-			  
-			  
-			  
 			  document.getElementById('xxPrimeSave').innerHTML = "S";
-			  
 			  
 			} else if (_isActive == true){
 				
-				
-				var geoJson = viewer.geoJsonData;
-				 document.getElementById('xxPrimeSave').innerHTML = "x x'"
-		        //_isActive = false;
+				 var geoJson = viewer.geoJsonData;
+				// document.getElementById('xxPrimeSave').innerHTML = "x x'"
+		         _isActive = false;
 			  
-			var dimArray =   viewer.dataSources._dataSources[0]._entityCollection._entities._array.length;
-			//console.log(dimArray);
+			     var dimArray =   viewer.dataSources._dataSources[0]._entityCollection._entities._array.length;
 			  
-			  var StringObj = "[";
 			  for(var i=0; i<dimArray; i++){
-			  	//StringObj += JSON.stringify(viewer.dataSources._dataSources[0]._entityCollection._entities._array[i]._properties)+",\r\n"
-				
-				geoJson.features[i].properties = viewer.dataSources._dataSources[0]._entityCollection._entities._array[i]._properties;
-				
-				console.log(geoJson.features[i].properties);
+				var modifiedPreperties = viewer.dataSources._dataSources[0]._entityCollection._entities._array[i]._properties;
+				geoJson.features[i].properties = modifiedPreperties;
+				geoJson.features[i].geometry.coordinates = [modifiedPreperties.center_lon, modifiedPreperties.center_lat];
 				
 			  }
-			  
-			  
-			  console.log(geoJson);
-			  
-			  // document.location = 'data:json/octet-stream;charset=utf-8,' +StringObj+"]";
-			   
-			  // saveAs(StringObj, "test.json");
+
+			  var downloadedData = 'data:json/octet-stream;charset=utf-8,' +JSON.stringify(geoJson, null, '\t');
+			
+			console.log(downloadedData);
+			
+			
+			  document.location = 'data:application/octet-stream;charset=utf-8,' +JSON.stringify(geoJson, null, '\n');  
 			   
 			}
         }
@@ -175,7 +139,7 @@ var _isActive = false;
                *
                * @type {String}
                */
-               this.tooltip = 'save geoJson file';
+               this.tooltip = 'Marker move';
                knockout.track(this, ['tooltip']);
 			   
            };
